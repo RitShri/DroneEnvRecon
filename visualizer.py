95ea9593c19f13c71eac14073f38d64de52a4c96
RitShri <ritishri@berkely.edu>
2022-04-13 15:10:12 -0700

    Found R and T for the right camera and added visualization code

---
+++ visualizer.py
@@ -0,0 +1,16 @@
+import numpy as np
+import open3d as o3d
+
+print("Load a ply point cloud, print it, and render it")
+pcd = o3d.io.read_point_cloud("Cloud_0.ply")
+
+o3d.visualization.draw_geometries([pcd],
+                                  zoom=0.3412,
+                                  front=[0.7, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
+                                  
+#pcd.points = o3d.utility.Vector3dVector(your_pointCloud)
+#pcd.colors = o3d.utility.Vector3dVector(np_colors)
+#o3d.visualization.draw_geometries([pcd])
+#
